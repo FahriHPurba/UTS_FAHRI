418f573e03a41a85cb0b68c84a6a54351a502864
FahriHPurba <fahrihiba1@gmail.com>
2021-04-22 17:19:50 +0700

    Projek Uts

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::resource('/D111811007_news', D111811007_newsController::class);
+Route::resource('/D111811007_admin', D111811007_adminController::class);
+
